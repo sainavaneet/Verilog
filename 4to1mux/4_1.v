4762a28af472700d6f09bd7dda08ea26ff227daa
Navaneet <126859932+sainavaneet@users.noreply.github.com>
2023-04-18 23:34:30 +0900

    Update 4_1.v

--- 4to1mux/4_1.v
+++ 4to1mux/4_1.v
@@ -1,7 +1,7 @@
 module mux_4_1(input [3:0] a, input [3 :0] b, input [3 :0] c, input [3 :] d, input [1 :0] se1, output [3 :0] out);
 
 
-	assign out = se[1] ? (se[0] ? d : c ) : (se1[0] ? b : a );
+	assign out = se[1] ? (se[0] ? d : c ) : (se1[0] ? b : a ); // this is using terniary operations
 
 
 endmodule 
